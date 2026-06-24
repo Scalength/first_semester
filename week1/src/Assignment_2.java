cb5e05ec43630482a10e4e67957537710d749b91
Scalength <m4gise4n@gmail.com>
2026-06-24 09:59:31 +0800

    WIP

--- week1/src/Assignment_2.java
+++ week1/src/Assignment_2.java
@@ -1,32 +1,33 @@
-// import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-// import java.util.Scanner;
+import java.util.Scanner;
 
 
 class Tindahan {
     Map<String, Integer> cash_register = new HashMap<>();
-    Tindahan tindahan = new Tindahan();
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> binili = new ArrayList<>();
+        Tindahan tindahan = new Tindahan();
 
-    tindahan.foo();
-    // public static void main(String[] args) {
-    //     Scanner scanner = new Scanner(System.in);
-    //     ArrayList<String> binili = new ArrayList<>();
+        tindahan.foo();
 
-    //     System.out.println("Ano pangalan mo iho? ");
-    //     String pangalan = scanner.nextLine();
+        System.out.println("Ano pangalan mo iho? ");
+        String pangalan = scanner.nextLine();
 
-    //     while (true) {
-    //         System.out.println("Enter your bibilin: ");
-    //         String paninda = scanner.nextLine();
-    //         binili.add(paninda);
-    //         if (paninda.equalsIgnoreCase("exit")) {
-    //             break;
-    //         }
-    //     }
-    //     System.out.println("Ang binili ni " + pangalan + " ay " + binili);
-    //     scanner.close();
-    // }
+        while (true) {
+            System.out.println("Enter your bibilin: ");
+            String paninda = scanner.nextLine();
+            binili.add(paninda);
+            if (paninda.equalsIgnoreCase("exit")) {
+                break;
+            }
+        }
+        System.out.println("Ang binili ni " + pangalan + " ay " + binili);
+        scanner.close();
+    }
 
     public void foo() {
         cash_register.put("Tite", 5 );
